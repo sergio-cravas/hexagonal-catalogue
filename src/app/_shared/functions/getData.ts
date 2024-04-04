1cab454731cdb6336d1290de81f77e242752381a
Sergio Cerdá Hervás <sergio.cravas@gmail.com>
2024-04-04 08:50:30 +0200

    fix: Applied properly hex arch at page function

--- src/app/_shared/functions/getData.ts
+++ src/app/_shared/functions/getData.ts
@@ -1,7 +1,8 @@
+import { getAllProducts } from '@/src/modules/products/arquitecture/getAllProducts';
 import { createRestProductRepository } from '@/src/modules/products/infraestruture/RestProductRepository';
 
 export const getData = async () => {
   const productsRepository = createRestProductRepository();
 
-  return productsRepository.getAll();
+  return getAllProducts(productsRepository)();
 };
